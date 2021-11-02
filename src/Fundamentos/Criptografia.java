787387b5d15edd53d684d1860bae09819fd976ea
luanlsr <luanfswd@gmail.com>
2021-11-02 16:18:24 -0300

    ifElse

--- src/Fundamentos/Criptografia.java
+++
@@ -1,9 +0,0 @@
-package Fundamentos;
-
-import Fundamentos.cripto.CriptografiaCesariana;
-
-public class Criptografia {
-    public static void main(String[] args) {
-        CriptografiaCesariana("A");
-    }
-}
